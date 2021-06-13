eabde68b8b3a577392944568c395f1f1ba4045b4
Rafi-99 <rafi.md.2018@gmail.com>
2021-06-12 22:29:50 -0500

    Updated to fetch almost any subreddit

--- index.js
+++ index.js
@@ -1,85 +1,75 @@
-const cors = require('cors')
+const cors = require('cors');
 const express = require('express');
 const got = require('got');
 const app = express();
-let port = process.env.PORT;
+const port = process.env.PORT;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send("Success!");
+    res.json({
+        "status": 200, 
+        "message": "Success!"
+    });
 });
 
-app.get('/memes', (req, res) => {
-    got('https://www.reddit.com/r/memes/random/.json').then(response => {
+app.get('/api/:subreddit', (req, res) => {
+    const subreddit = req.params.subreddit;
+    
+    got(`https://www.reddit.com/r/${subreddit}/random/.json`).then(response => {
         let meme = JSON.parse(response.body);
-        let title = meme[0].data.children[0].data.title;
-        let linkIdentifier = meme[0].data.children[0].data.permalink;
-        let url = 'https://www.reddit.com' + linkIdentifier;
-        let image = meme[0].data.children[0].data.url;
-        let upvotes = meme[0].data.children[0].data.ups;
-        let downvotes = meme[0].data.children[0].data.downs;
-        let comments = meme[0].data.children[0].data.num_comments;
 
-        var memeData = {
-            title:title, 
-            url:url,
-            image:image,
-            upvotes:upvotes,
-            downvotes:downvotes,
-            comments:comments
-        };
-
-        var memeDataFormat = {
-            success:true, 
-            data:memeData
-        };
-
-        var string = JSON.stringify(memeDataFormat);
-        var output = JSON.parse(string);
-
-        res.json(output);
+        if(meme[0] === undefined) {
+            res.json({
+                "success": false,
+                "message": 'The requested resouce cannot be fetched or does not exist.'
+            });
+        }
         
-    }).catch(console.log);
-});
+        else {
+            let title = meme[0].data.children[0].data.title;
+            let linkIdentifier = meme[0].data.children[0].data.permalink;
+            let url = 'https://www.reddit.com' + linkIdentifier;
+            let image = meme[0].data.children[0].data.url;
+            let upvotes = meme[0].data.children[0].data.ups;
+            let downvotes = meme[0].data.children[0].data.downs;
+            let comments = meme[0].data.children[0].data.num_comments;
 
-app.get('/dankmemes', (req, res) => {
-    got('https://www.reddit.com/r/dankmemes/random/.json').then(response => {
-        let meme = JSON.parse(response.body);
-        let title = meme[0].data.children[0].data.title;
-        let linkIdentifier = meme[0].data.children[0].data.permalink;
-        let url = 'https://www.reddit.com' + linkIdentifier;
-        let image = meme[0].data.children[0].data.url;
-        let upvotes = meme[0].data.children[0].data.ups;
-        let downvotes = meme[0].data.children[0].data.downs;
-        let comments = meme[0].data.children[0].data.num_comments;
+            let memeData = {
+                title:title, 
+                url:url,
+                image:image,
+                upvotes:upvotes,
+                downvotes:downvotes,
+                comments:comments
+            };
 
-        var memeData = {
-            title:title, 
-            url:url,
-            image:image,
-            upvotes:upvotes,
-            downvotes:downvotes,
-            comments:comments
-        };
+            let memeDataFormat = {
+                success:true, 
+                data:memeData
+            };
 
-        var memeDataFormat = {
-            success:true, 
-            data:memeData
-        };
-
-        var string = JSON.stringify(memeDataFormat);
-        var output = JSON.parse(string);
-
-        res.json(output);
-        
-    }).catch(console.log);
-});
+            res.json(memeDataFormat); 
+        }
+    }).catch(error => {
+        let errorHandler = new Promise((resolve, reject) => {
+            reject(error);
+            throw res.json({
+                "success": false,
+                "message": 'The requested resouce cannot be fetched or does not exist.'
+            });
+        });
+        errorHandler.catch(error => {/* Error handled. */});
+    });
+}); 
 
 app.get("*", (req, res) => {
-    res.send("Error! The page you are looking for does not exist.");
+    res.json({
+        "status": 404,
+        "message": 'Page not found.'
+    });
 });
 
 app.listen(port, () => {
-    console.log(`App is listening on http://localhost:${port}`);
+    console.log(`App is listening on port ${port}.`);
 });
